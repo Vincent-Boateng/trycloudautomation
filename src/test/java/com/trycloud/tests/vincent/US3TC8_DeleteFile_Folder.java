e5fa1a8eefe28bc00a45ec0fc0fc961eef4ab06e
Vincent-Boateng <vincentboateng@gmail.com>
2021-02-04 12:05:59 -0500

    Did a little work  on DeleteFile Class

--- src/test/java/com/trycloud/tests/vincent/US3TC8_DeleteFile_Folder.java
+++ src/test/java/com/trycloud/tests/vincent/US3TC8_DeleteFile_Folder.java
@@ -1,9 +1,21 @@
 package com.trycloud.tests.vincent;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
-public class US3TC8_DeleteFile_Folder extends TestBase {
+public class US3TC8_DeleteFile_Folder  extends TestBase {
 
+@Test
+    public static void click_ActionIcon_FromFile_test(){
+
+    WebElement action_icon = WebDriverFactory.getDriver().findElement(By.xpath(""));
+
+
+
+}
 
 
 }
