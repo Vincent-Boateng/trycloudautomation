7b3c17622c8819caa4dc9e1fe60f6f802b0ec118
MVK-USA <mvk2015@yahoo.com>
2021-02-04 11:35:57 -0500

    New classes for user stories

---
+++ src/test/java/com/trycloud/tests/mariia/US4TC1_users_can_access_to_Talks_module.java
@@ -0,0 +1,7 @@
+package com.trycloud.tests.mariia;
+
+import com.trycloud.tests.base.TestBase;
+
+public class US4TC1_users_can_access_to_Talks_module extends TestBase {
+
+}
---
+++ src/test/java/com/trycloud/tests/mariia/US4TC2_users_can_send_messages.java
@@ -0,0 +1,4 @@
+package com.trycloud.tests.mariia;
+
+public class US4TC2_users_can_send_messages {
+}
