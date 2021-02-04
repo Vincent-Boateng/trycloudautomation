c8aa903c2644bc9857996fb5cdbd14238996aed4
sirinapha-goe <thenutzg@gmail.com>
2021-02-04 13:01:40 -0500

    I can see

---
+++ src/test/java/com/trycloud/tests/sirinapha/US3TC6_CreateFolder.java
@@ -0,0 +1,15 @@
+package com.trycloud.tests.sirinapha;
+
+import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class US3TC6_CreateFolder extends TestBase {
+    @Test
+    public static void click_NotesModule() {
+
+
+    }
+
+}
