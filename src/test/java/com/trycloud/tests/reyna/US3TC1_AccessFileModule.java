111596e080685a5025db53013804b4754fa3562c
Reyna Navarro <navarroreyna1976@gmail.com>
2021-02-04 12:35:58 -0500

    commiting on first TC

--- src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
+++ src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
@@ -1,8 +1,9 @@
 package com.trycloud.tests.reyna;
 
 import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
 
 public class US3TC1_AccessFileModule extends TestBase {
 
-    
+
 }
