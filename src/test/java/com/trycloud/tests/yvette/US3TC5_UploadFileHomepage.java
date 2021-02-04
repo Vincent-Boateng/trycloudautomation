09e19b2d3e1b32761c651ea2db521fe82cce01f7
NicoleYvetteBasta <yvette.basta@gmail.com>
2021-02-04 12:10:59 -0500

    started @test

--- src/test/java/com/trycloud/tests/yvette/US3TC5_UploadFileHomepage.java
+++ src/test/java/com/trycloud/tests/yvette/US3TC5_UploadFileHomepage.java
@@ -1,6 +1,10 @@
 package com.trycloud.tests.yvette;
 
 import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
 
 public class US3TC5_UploadFileHomepage extends TestBase {
+
+//@Test
+//    public static void
 }
