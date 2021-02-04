1bf567c6f09ba722d8429b9991b2db6e050d171a
AfroozPoorhashemi <afrooz1353@yahoo.com>
2021-02-04 11:13:03 -0500

    Create a new class

---
+++ src/test/java/com/trycloud/tests/afrooz/US3TC9_ChangeAppSettings.java
@@ -0,0 +1,11 @@
+package com.trycloud.tests.afrooz;
+
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class US3TC9_ChangeAppSettings extends TestBase {
+
+       @Test
+
+
+}
