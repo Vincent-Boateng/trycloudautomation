c9f8b67c7ed21b412f9ec9bc59df3745046ee306
busrabkn <74192539+busrabkn@users.noreply.github.com>
2021-02-04 12:05:50 -0500

    work in new class US2TC1_VerifyAccessMainModules

--- src/test/java/com/trycloud/tests/busra/US2TC1_VerifyAccessMainModules.java
+++ src/test/java/com/trycloud/tests/busra/US2TC1_VerifyAccessMainModules.java
@@ -1,8 +1,19 @@
 package com.trycloud.tests.busra;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class US2TC1_VerifyAccessMainModules extends TestBase {
 
+    @Test
+    public static void VerifytheURLischawedtohomepageurl(){
+
+        WebElement a = WebDriverFactory.getDriver().findElement(By.xpath(""));
+
+
+    }
 
 }
