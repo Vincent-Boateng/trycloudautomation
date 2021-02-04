1e542e771b3c26ed07cdf2b24d9973bf8ea79c09
Batch-21 ForLearning <samadigithub@gmail.com>
2021-02-04 13:32:56 -0500

    This is the second class for the second Test Case

--- src/test/java/com/trycloud/tests/sayed/US6TC2_Verify_Sending_NotesInThePage.java
+++ src/test/java/com/trycloud/tests/sayed/US6TC2_Verify_Sending_NotesInThePage.java
@@ -1,5 +1,8 @@
 package com.trycloud.tests.sayed;
 
-public class US6TC2_Verify_Sending_NotesInThePage {
+import com.trycloud.tests.base.TestBase;
+
+public class US6TC2_Verify_Sending_NotesInThePage extends TestBase {
+    // Here to write your code
 
 }
