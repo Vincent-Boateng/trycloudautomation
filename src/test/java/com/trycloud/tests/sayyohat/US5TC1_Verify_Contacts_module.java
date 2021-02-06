14a48e36a7fe718634aab526828a762dcf472c80
Sayyohat Bekmurodova <bekmurodovas@gmail.com>
2021-02-06 18:29:30 -0500

    added codes

--- src/test/java/com/trycloud/tests/sayyohat/US5TC1_Verify_Contacts_module.java
+++ src/test/java/com/trycloud/tests/sayyohat/US5TC1_Verify_Contacts_module.java
@@ -1,6 +1,8 @@
 package com.trycloud.tests.sayyohat;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,9 +11,13 @@ import org.testng.annotations.Test;
 public class US5TC1_Verify_Contacts_module extends TestBase {
 
 @Test
-    public static void click_ContactsModule_FromFile_test(){
+    public static void click_ContactsModule_FromFile_test() throws InterruptedException {
 
-    WebElement contacts_module = WebDriverFactory.getDriver().findElement(By.xpath(""));
+    WebElement contacts_module = WebDriverFactory.getDriver().findElement(By.linkText("Contacts"));
+    BrowserUtils.sleep(3);
+    contacts_module.click();
+    BrowserUtils.sleep(3);
+
 
 }
 }
