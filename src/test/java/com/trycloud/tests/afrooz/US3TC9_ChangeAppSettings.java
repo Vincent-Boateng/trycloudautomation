1876fd2f27ebfff7df6fdf881c06a4fab76e346a
AfroozPoorhashemi <afrooz1353@yahoo.com>
2021-02-06 15:16:29 -0500

    New Codes added

--- src/test/java/com/trycloud/tests/afrooz/US3TC9_ChangeAppSettings.java
+++ src/test/java/com/trycloud/tests/afrooz/US3TC9_ChangeAppSettings.java
@@ -13,15 +13,36 @@ public class US3TC9_ChangeAppSettings extends TestBase {
 
        @Test
 
-       public static void click_Setting_bottom_test() {
+       public static void click_Setting_bottom_test() throws InterruptedException {
 
-              WebElement click_U = WebDriverFactory.getDriver().findElement(By.xpath("//div[@class='avatardiv avatardiv-shown']//img"));
+              WebElement click_Files = WebDriverFactory.getDriver().findElement(By.xpath("//ul[@id='appmenu']//a[@aria-label='Files']//*[local-name()='svg']//*[name()='image' and contains(@class,'app-icon')]"));
 
-              click_U.click();
+              click_Files.click();
 
-              WebElement click_Activity= WebDriverFactory.getDriver().findElement(By.xpath("//a[normalize-space()='Settings']"));
+              WebElement click_Settings= WebDriverFactory.getDriver().findElement(By.xpath("//button[normalize-space()='Settings']"));
 
-              click_Activity.click();
+              click_Settings.click();
+
+              Thread.sleep(2000);
+
+              WebElement click_Sharing= WebDriverFactory.getDriver().findElement(By.xpath("//span[normalize-space()='Sharing']"));
+
+              click_Sharing.click();
+
+              Thread.sleep(2000);
+
+              WebElement checkbox3 = WebDriverFactory.getDriver().findElement(By.xpath("//label[normalize-space()='Show hidden files']"));
+
+              checkbox3.click();
+
+              if (!checkbox3.isSelected() && !checkbox3.isDisplayed()){
+                     System.out.println("Checkbox is not selected. Message is not displayed. Verification PASS!");
+              }else{
+                     System.out.println("FAIL!");
+              }
+
+
+              Thread.sleep(3000);
 
 
        }
