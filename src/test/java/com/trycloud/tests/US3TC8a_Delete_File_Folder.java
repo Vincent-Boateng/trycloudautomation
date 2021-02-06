5a20071021d790b6252153f394268ace28fce4ed
Vincent-Boateng <vincentboateng@gmail.com>
2021-02-05 20:39:09 -0500

    I added annotations, priorities and groups

--- src/test/java/com/trycloud/tests/US3TC8a_Delete_File_Folder.java
+++ src/test/java/com/trycloud/tests/US3TC8a_Delete_File_Folder.java
@@ -3,9 +3,10 @@ package com.trycloud.tests;
 import com.trycloud.tests.base.TestBase;
 import org.testng.annotations.Test;
 
-public class US3TC8b_Delete_File_Folder extends TestBase {
+@Test(priority = 8, groups="userStory3")
+public class US3TC8a_Delete_File_Folder extends TestBase {
 
-    @Test
+    @Test(priority = 1, groups = {"regression", "smoke"})
     public void delete_file_feature_verification_test(){
 
 
