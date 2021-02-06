d8a88a70e30953753efb1d4d3ffc86071dacfc7a
Vincent-Boateng <vincentboateng@gmail.com>
2021-02-05 19:25:00 -0500

    I've started work on the delete file feature verification

---
+++ src/test/java/com/trycloud/tests/US3TC8b_Delete_File_Folder.java
@@ -0,0 +1,15 @@
+package com.trycloud.tests;
+
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class US3TC8b_Delete_File_Folder extends TestBase {
+
+    @Test
+    public void delete_file_feature_verification_test(){
+
+
+
+    }
+
+}
