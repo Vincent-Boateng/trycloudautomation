aa7401502632b2f1309013b3336a0e5073224edb
Sayyohat Bekmurodova <bekmurodovas@gmail.com>
2021-02-07 16:36:15 -0500

    Added some codes to Test case 1 and Test case 2

--- src/test/java/com/trycloud/tests/sayyohat/US5TC1_Verify_Contacts_module.java
+++ src/test/java/com/trycloud/tests/sayyohat/US5TC1_Verify_Contacts_module.java
@@ -10,9 +10,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import sun.management.snmp.jvmmib.JVM_MANAGEMENT_MIBOidTable;
 
 public class US5TC1_Verify_Contacts_module extends TestBase {
-
+//5. Story: As a user, I should be able to access to Contacts module.
     private static Chart driver;
 
     @Test
@@ -22,15 +23,32 @@ public class US5TC1_Verify_Contacts_module extends TestBase {
 2. Click “Contacts” module
 
  */
-    WebElement contactsModule = WebDriverFactory.getDriver().findElement(By.xpath("//a[@href='/index.php/apps/contacts/']"));
-    BrowserUtils.sleep(3);
-    contactsModule.click();
-    BrowserUtils.sleep(3);
+        WebElement contactsModule = WebDriverFactory.getDriver().findElement(By.xpath("//a[@href='/index.php/apps/contacts/']"));
+        BrowserUtils.sleep(3);
+        contactsModule.click();
+        BrowserUtils.sleep(3);
 
-  //  3. Verify the page tile is Contents module’s title
+ /*
+  //  3. Verify the page title is Contacts module’s title
     String actualTitle= driver.getTitle();
         String expectedTitle = "Contacts";
         Assert.assertTrue(actualTitle.contains(expectedTitle));
+  */
+    }
+        @Test
+                public void addContacts(){
+            //Test case #2 - verify users can add all contacts info
+            //2.Click contacts module
+            WebElement contactsModule = WebDriverFactory.getDriver().findElement(By.xpath("//a[@href='/index.php/apps/contacts/']"));
+            BrowserUtils.sleep(2);
+            contactsModule.click();
+
+            //3.Click “New Contact” button
+            WebElement newContactButton = WebDriverFactory.getDriver().findElement(By.id("new-contact-button"));
+            newContactButton.click();
+            BrowserUtils.sleep(2);
+
+        }
 
 }
-}
+
