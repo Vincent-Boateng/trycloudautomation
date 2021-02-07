d6c718ad7cf205a7a15d964ba2e8515d04387170
Sayyohat Bekmurodova <bekmurodovas@gmail.com>
2021-02-07 17:37:43 -0500

    Added comments

--- src/test/java/com/trycloud/tests/sayyohat/US5TC3_ContactList.java
+++ src/test/java/com/trycloud/tests/sayyohat/US5TC3_ContactList.java
@@ -1,4 +1,19 @@
 package com.trycloud.tests.sayyohat;
 
-public class US5TC3_ContactList {
+import com.google.common.base.Verify;
+import org.testng.annotations.Test;
+
+public class US5TC3_ContactList extends US5TC1TC2_Verify_Contacts_Add_Contacts {
+    //Test case #3 - verify users can see all the contact names on the contact list
+
+    @Test
+    public void ContactsList(){
+      //  1. Login as a user
+      //  2. Click contacts module
+      //  3. Verify the contact names are in the list
+
+        // Verified 2 contacts on the list
+
+    }
+
 }
