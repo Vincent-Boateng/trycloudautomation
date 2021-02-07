3c2d0cda0a48214bf8e829ae3006f777eb8a4038
Reyna Navarro <navarroreyna1976@gmail.com>
2021-02-07 16:11:44 -0500

    Tested login as user extended from TestBase code

--- src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
+++ src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
@@ -1,9 +1,17 @@
 package com.trycloud.tests.reyna;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class US3TC1_AccessFileModule extends TestBase {
 
+    @Test
+    public static void verifyUsersCanAccessToFilesModule() throws InterruptedException{
 
+
+    }
 }
