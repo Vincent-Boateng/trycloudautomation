bbd76911a38a4d9d3326d5f631fa5531c3cda60c
Vincent-Boateng <vincentboateng@gmail.com>
2021-02-08 21:26:43 -0500

    Just started work on User story 3 test case 3

---
+++ src/test/java/com/trycloud/tests/vincent/US3TC3_Verify_Add_File_To_Favorites.java
@@ -0,0 +1,14 @@
+package com.trycloud.tests.vincent;
+
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class US3TC3_Verify_Add_File_To_Favorites extends TestBase {
+
+    @Test
+    public void US3TC3_Add_File_To_Favorites(){
+
+
+
+    }
+}
