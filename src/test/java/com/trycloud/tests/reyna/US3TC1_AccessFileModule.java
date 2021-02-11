2cbe68660f0572aba3feefd486608ff95da06d36
Vincent-Boateng <vincentboateng@gmail.com>
2021-02-10 19:23:22 -0500

    User story 3, Test case 3 is done

--- src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
+++ src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
@@ -1,8 +1,13 @@
 package com.trycloud.tests.reyna;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class US3TC1_AccessFileModule extends TestBase {
 
-    
+
 }
