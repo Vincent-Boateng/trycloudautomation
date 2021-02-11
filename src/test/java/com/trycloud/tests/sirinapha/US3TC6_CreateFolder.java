01f6d23db064b09e6a64d16f38a5bd0e3b20cde6
Vincent-Boateng <vincentboateng@gmail.com>
2021-02-10 19:16:33 -0500

    User story 3, Test cases 6 and 7 are done

--- src/test/java/com/trycloud/tests/sirinapha/US3TC6_CreateFolder.java
+++ src/test/java/com/trycloud/tests/sirinapha/US3TC6_CreateFolder.java
@@ -1,13 +1,39 @@
 package com.trycloud.tests.sirinapha;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US3TC6_CreateFolder extends TestBase {
     @Test
-    public static void click_NotesModule() {
+    public void verify_user_can_create_folder_test(){
+
+        WebElement filesTab = WebDriverFactory.getDriver().findElement(By.xpath("//ul[@id='appmenu']//a[@aria-label='Files']//*[local-name()='svg']//*[name()='image' and contains(@class,'app-icon')]"));
+        BrowserUtils.sleep(5);
+        filesTab.click();
+        BrowserUtils.sleep(5);
+
+        WebElement plusSign = WebDriverFactory.getDriver().findElement(By.xpath("//span[@class='icon icon-add']"));
+        plusSign.click();
+
+        WebElement newFolderTab = WebDriverFactory.getDriver().findElement(By.xpath("//*[@id=\"controls\"]/div[2]/div[2]/ul/li[2]/a/span[2]"));
+        newFolderTab.click();
+
+        WebElement inputFolder  = WebDriverFactory.getDriver().findElement(By.xpath("//input[@id='view13-input-folder']"));
+        inputFolder.sendKeys("Finance");
+
+        WebElement submitButton = WebDriverFactory.getDriver().findElement(By.xpath("//*[@id=\"controls\"]/div[2]/div[2]/ul/li[2]/a/form/input[2]"));
+
+        submitButton.click();
+
+        WebElement financeFolder = WebDriverFactory.getDriver().findElement(By.xpath("//*[@id=\"app-sidebar-vue\"]/header/div[1]/div[2]/div[1]/a"));
+
+        Assert.assertTrue(financeFolder.isDisplayed());
+
 
 
     }
