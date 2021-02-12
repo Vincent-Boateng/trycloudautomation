fc3ac26261ce8edfbe607988b11d4296e14b7b28
Reyna Navarro <navarroreyna1976@gmail.com>
2021-02-11 20:41:36 -0500

    Finished US3TC1_AccessFileModule

--- src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
+++ src/test/java/com/trycloud/tests/reyna/US3TC1_AccessFileModule.java
@@ -5,13 +5,19 @@ import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US3TC1_AccessFileModule extends TestBase {
 
     @Test
-    public static void verifyUsersCanAccessToFilesModule() throws InterruptedException{
+    public void verifyUsersCanAccessToFilesModule(){
+        WebElement fileTabElement = WebDriverFactory.getDriver().findElement(By.xpath("//ul[@id='appmenu']//a[@aria-label='Files']//*[local-name()='svg']//*[name()='image' and contains(@class,'app-icon')]"));
+        fileTabElement.click();
 
+        String expectedTitle = "Files - Trycloud";
+        String actualTitle = WebDriverFactory.getDriver().getTitle();
 
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 }
